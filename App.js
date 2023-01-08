1d51e6a3f04a218ab3bc03f8b96f135e3e980f84
Jokin Irizar Melero <jokin.irizar@ikasle.aeg.eus>
2023-01-08 21:12:49 +0100

    error styled components

--- App.js
+++ App.js
@@ -1,13 +1,97 @@
+import { StyleSheet, Text, View} from 'react-native';
+import React  from 'react';
+import { useState, useEffect } from 'react';
+import { Flatlist } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import { useFonts } from 'expo-font';
+import  styled from 'styled-components/native';
+import Rating from './components/Rating';
+import Genre from './components/Genre';
+import { getMovies } from './api';
+import * as CONSTANTS from './constants/constants'
 
+const Container = styled.View` 
+flex: 1;
+`;
+const PosterContainer = styled.View` 
+width: ${CONSTANTS.ITEM_SIZE}px;
+`;
+const Poster = styled.View`
+margin-horizontal : ${CONSTANTS.SPACING}px;
+padding: ${CONSTANTS.SPACING * 2}px;
+align-items: center;
+background-color: #FFFFFF;
+border-radius: 10px;
+`;
+const PosterImage = styled.Image`
+width: 100%;
+height: ${CONSTANTS.ITEM_SIZE * 1.2}px;
+resize-mode: cover;
+border-radius: 10px;
+margin: 0 0 10px 0;
+`;
+
+const PosterTitle = styled.Text`
+font-family: Inter-Black;
+font-size: 18px;
+`;
+const PosterDescription = styled.Text`
+font-family: ;
+font-size: 18;
+font-size: 12px;
+`;
 export default function App() {
+
+  const [movies, setMovies] = useState([])
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getMovies()
+      setMovies(data)
+      setLoaded(true)
+    }
+    fetchData()
+  }, [])
+
+  let [fontLoaded] = useFonts({
+    'Inter-Black': require('./assets/fonts/Inter-Black.ttf')
+  })
+
+  if (!loaded || !fontLoaded) {
+    return <AppLoading />
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <StatusBar />
+      <FlatList
+        showsHorizontalScrollIndicator={false}
+        data={movies}
+        keyExtractor={item => item.key}
+        horizontal
+        contentContainerStyle={{
+          alignItems: 'center'
+        }}
+
+        renderItem={({ item }) => {
+          return (
+            <PosterContainer>
+              <Poster>
+                <PosterImage source={{ uri: item.posterPath }} />
+                <PosterTitle numberOfLines={1}>{item.originalTitle}</PosterTitle>
+                <Rating rating={item.voteAverage} />
+                <Genre genres={item.genres} />
+                <PosterDescription numberOfLines={5}>{item.description}</PosterDescription>
+              </Poster>
+            </PosterContainer>
+          )
+        }}
+      />
+    </Container>
   );
+
+
 }
 
 const styles = StyleSheet.create({
---
+++ api.js
@@ -0,0 +1,35 @@
+import { API_KEY, GENRES } from "./config";
+
+const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc`
+
+export const getMovies = async () => {
+    try {
+        let response = await fetch(API_URL)
+        let json = await response.json();
+        const movies = json.results.map(
+            ({
+                id,
+                original_title,
+                poster_path,
+                backdrop_path,
+                vote_average,
+                overview,
+                release_date,
+                genre_ids,
+            }) => ({
+                key: String(id),
+                originalTitle: original_title,
+                posterPath: `https://image.tmdb.org/t/p/w500${poster_path}`,
+                backdropPath: `https://image.tmdb.org/t/p/w500${backdrop_path}`,
+                voteAverage: vote_average,
+                description: overview,
+                releaseDate: release_date,
+                genres: genre_ids.map(id => GENRES[id])
+            })
+        )
+
+        return movies
+    } catch (error) {
+        Console.error(error);
+    }
+}
